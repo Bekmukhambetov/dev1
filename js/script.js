d075059c0595115b404680cd1ee19d09d6bd70e6
Vadym <107219053+Bekmukhambetov@users.noreply.github.com>
2023-12-12 10:09:56 +0200

    modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
 function getData() {
-<<<<<<< HEAD
-    fetch('http://localhost:5000/api/data')
-=======
-    fetch('http://127.0.0.1:5000/api/data')
->>>>>>> cf18f98bea02dd235c7452f522bea58d120cae4d
 
+    fetch('http://127.0.0.1:5000/api/data')
+
+    
         .then(response => response.json())
         .then(data => {
             document.getElementById('backendResponse').innerText = data.message;
